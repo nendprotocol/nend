f73bd6dcbf07030277228c90f36275661ae0667e
Richard-nend <richard.kim@nendfi.com>
2025-04-05 11:28:55 +0900

    applied map-based active tracking for taking ids in LendingPollStakingV2

--- deploy/contractsExecute/executeNendSetStaking.ts
+++ deploy/contractsExecute/executeNendSetStaking.ts
@@ -1,17 +1,16 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction, Deployment} from 'hardhat-deploy/types';
-const {ethers} = require("hardhat");
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction, Deployment } from 'hardhat-deploy/types';
 import version from '../version';
 import retry from '../retry';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const {deployments, getNamedAccounts, getChainId} = hre;
-    const {deploy, execute} = deployments;
-    const {deployer} = await getNamedAccounts();
-    const ChainId = await getChainId();
+  const { deployments, getNamedAccounts, getChainId } = hre;
+  const { deploy, execute } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const ChainId = await getChainId();
 
     interface DeploymentExt extends Deployment {
-        newlyDeployed?: boolean; 
+        newlyDeployed?: boolean;
     }
 
     const mainnet = version.mainnet;
@@ -21,29 +20,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const TurboSalt = `nend-turbo-v${version.number}`;
 
     const NendDeployment : DeploymentExt = await deployments.get('NEND');
-    const StakingDeployment : DeploymentExt = await deployments.get('Staking');
-    
-    if (NendDeployment.newlyDeployed === undefined || StakingDeployment.newlyDeployed === undefined ) {  
+    const StakingDeployment : DeploymentExt = await deployments.get((ChainId === '137' ? 'StakingPool' : 'Staking'));
 
-        while(true) {
-            try {
-              const NendDeploymentDeploymentSetStaking = await execute('NEND',
-                {from: deployer, log: true},
-                'setStaking', StakingDeployment.address
-              );	
-              break;
-            }catch(err) {
-              console.log(err);
-              console.log('Transaction failed');
-              await retry();
-            }
+    if (NendDeployment.newlyDeployed === undefined || StakingDeployment.newlyDeployed === undefined) {
+      while (true) {
+        try {
+          const NendDeploymentDeploymentSetStaking = await execute('NEND',
+            { from: deployer, log: true },
+            'setStaking', StakingDeployment.address
+          );
+          break;
+        } catch (err) {
+          console.log(err);
+          console.log('Transaction failed');
+          await retry();
         }
-
+      }
     }
-
-}
+};
 
 export default func;
-func.tags = [ 'NendSetStaking' ];
+func.tags = ['NendSetStaking'];
 module.exports.dependencies = ['NendDeployment', 'StakingDeployment'];
 module.exports.runAtTheEnd = true;
